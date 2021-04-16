efb6273cbb8ac721c202de15ccd4dd223b0d6393
Michael Davidsaver <mdavidsaver@gmail.com>
2021-04-16 08:52:49 -0700

    fix

--- test1.sh
+++ test1.sh
@@ -6,5 +6,6 @@ echo "Testing"
 echo "::warning file=blah.h,line=42,col=0::Hello World"
 echo "::warning file=blah.h,line=43::Testing"
 echo "::error file=blah.h,line=44,col=0::oops"
+echo "::error::Something went wrong"
 
 exit 0
