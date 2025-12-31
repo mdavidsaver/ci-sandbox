e764f3b6346c1a373081fc6b5af937d7e5b85aa6
Michael Davidsaver <mdavidsaver@gmail.com>
2025-12-31 14:45:10 -0800

    more

--- installcdb.py
+++ installcdb.py
@@ -1,7 +1,5 @@
-import os
 import sys
 import logging
-import shutil
 import winreg
 from subprocess import check_call
 
@@ -22,55 +20,59 @@ rootname = r'Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
 
 # find latest version of winsdksetup.exe
 
-winsdks = {}
+def main():
+    winsdks = {}
 
-with winreg.OpenKeyEx(winreg.HKEY_LOCAL_MACHINE, rootname, 0, access) as root:
-    nsub, _nval, _lastmod = winreg.QueryInfoKey(root)
+    with winreg.OpenKeyEx(winreg.HKEY_LOCAL_MACHINE, rootname, 0, access) as root:
+        nsub, _nval, _lastmod = winreg.QueryInfoKey(root)
 
-    for idx in range(nsub):
-        sname = winreg.EnumKey(root, idx) # some hash or GUID
+        for idx in range(nsub):
+            sname = winreg.EnumKey(root, idx) # some hash or GUID
 
-        with winreg.OpenKeyEx(root, sname, 0, access) as sk:
-            try:
-                sval, _stype = winreg.QueryValueEx(sk, 'DisplayName') # "Windows Software Development Kit - Windows 10.0.26100.7175"
-            except FileNotFoundError:
-                continue # ignore entries w/o DisplayName
-            except:
-                logging.exception(f'{rootname}\\{sname} DisplayName')
-                continue
-            else:
-                print(f'  consider {sname} : {sval!r}')
-                if not sval.startswith('Windows Software Development Kit'):
-                    continue
-
-                print(f'Found Windows SDK {sname} : {sval!r}')
-
+            with winreg.OpenKeyEx(root, sname, 0, access) as sk:
                 try:
-                    iversion, _vtype = winreg.QueryValueEx(sk, 'DisplayVersion') # eg. "10.1.26100.7175"
-                    print(f'  version: {iversion}')
-                    installer, _itype = winreg.QueryValueEx(sk, 'BundleCachePath') # "C:\ProgramData\...\winsdksetup.exe"
-                    print(f'  installer: {installer}')
-
-                    winsdks[iversion] = installer
+                    sval, _stype = winreg.QueryValueEx(sk, 'DisplayName') # "Windows Software Development Kit - Windows 10.0.26100.7175"
+                except FileNotFoundError:
+                    continue # ignore entries w/o DisplayName
                 except:
-                    logging.exception(sname)
+                    logging.exception(f'{rootname}\\{sname} DisplayName')
+                    continue
+                else:
+                    print(f'  consider {sname} : {sval!r}')
+                    if not sval.startswith('Windows Software Development Kit'):
+                        continue
 
-if not len(winsdks):
-    raise RuntimeError('No winsdksetup.exe')
+                    print(f'Found Windows SDK {sname} : {sval!r}')
 
-winsdks = list(winsdks.items()) # [(version, exe)]
-winsdks.sort() # sort incrementing
+                    try:
+                        iversion, _vtype = winreg.QueryValueEx(sk, 'DisplayVersion') # eg. "10.1.26100.7175"
+                        print(f'  version: {iversion}')
+                        installer, _itype = winreg.QueryValueEx(sk, 'BundleCachePath') # "C:\ProgramData\...\winsdksetup.exe"
+                        print(f'  installer: {installer}')
 
-for ver, exe in winsdks:
-    print(f'{ver}: {exe!r}')
+                        winsdks[iversion] = installer
+                    except:
+                        logging.exception(sname)
 
-winsdk = winsdks[-1][1] # last (latest)
+    if not len(winsdks):
+        raise RuntimeError('No winsdksetup.exe')
 
-cmd = [
-    winsdk,
-    '/features', 'OptionId.WindowsDesktopDebuggers', '/q',
-]
-print(f'run: {cmd!r}')
-sys.stdout.flush()
-sys.stderr.flush()
-check_call(cmd)
+    winsdks = list(winsdks.items()) # [(version, exe)]
+    winsdks.sort() # sort incrementing
+
+    for ver, exe in winsdks:
+        print(f'{ver}: {exe!r}')
+
+    winsdk = winsdks[-1][1] # last (latest)
+
+    cmd = [
+        winsdk,
+        '/features', 'OptionId.WindowsDesktopDebuggers', '/q',
+    ]
+    print(f'run: {cmd!r}')
+    sys.stdout.flush()
+    sys.stderr.flush()
+    check_call(cmd)
+
+if __name__=='__main__':
+    main()
